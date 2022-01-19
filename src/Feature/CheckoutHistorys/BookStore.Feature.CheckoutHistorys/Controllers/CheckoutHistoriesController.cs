393a4a3846b0b896c8d47fba1254c4679521c19c
Vladimir Sharov <Vladimir_Sharov@epam.com>
2022-01-19 17:38:27 +0300

    Create Web MVC project CheckoutHistories

---
+++ src/Feature/CheckoutHistorys/BookStore.Feature.CheckoutHistorys/Controllers/CheckoutHistoriesController.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Feature.CheckoutHistorys.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Feature.CheckoutHistorys.Controllers
+{
+  public class CheckoutHistoriesController : Controller
+  {
+    private readonly ILogger<CheckoutHistoriesController> _logger;
+
+    public CheckoutHistoriesController(ILogger<CheckoutHistoriesController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
