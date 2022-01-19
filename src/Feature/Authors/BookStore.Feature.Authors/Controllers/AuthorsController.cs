66fd80bc981ef723c943b3b2177244f62d140628
Vladimir Sharov <Vladimir_Sharov@epam.com>
2022-01-19 17:48:20 +0300

    Create Web MVC project Authors

---
+++ src/Feature/Authors/BookStore.Feature.Authors/Controllers/AuthorsController.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Feature.Authors.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Feature.Authors.Controllers
+{
+  public class AuthorsController : Controller
+  {
+    private readonly ILogger<AuthorsController> _logger;
+
+    public AuthorsController(ILogger<AuthorsController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
