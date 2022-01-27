f82488360d2774b6a31c5250700bb827c6a62e74
Vladimir Sharov <Vladimir_Sharov@epam.com>
2022-01-27 12:58:44 +0300

    Project View Model CheckoutHistorys  * Create a view model CheckoutHistorys  * Create a view CheckoutHistorys  * Create a CheckoutHistorys's Controller  * Create a Profile AutoMapper binding to DTO Data DB model

---
+++ src/Feature/CheckoutHistorys/BookStore.Feature.CheckoutHistorys/Models/IndexViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Feature.CheckoutHistorys.Models
+{
+  public class IndexViewModel
+  {
+    public string Id { get; set; }
+
+    public ClientReduced Client { get; set; }
+
+    public BookReduced Book { get; set; }
+
+    public DateTime CheckoutDate { get; set; }
+
+    public DateTime? ReturnDate { get; set; }
+  }
+
+  public class ClientReduced
+  {
+    public string Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string FullName
+    {
+      get { return LastName + " " + FirstName; }
+    }
+  }
+
+  public class BookReduced
+  {
+    public string ISBN { get; set; }
+
+    public string Name { get; set; }
+  }
+}
